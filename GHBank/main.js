1eaadc844b9fd666c40a191b4d4d467fc7a6a87a
Gabriel Hiago de Sousa <gabrielhiago96@hotmail.com>
2024-01-03 11:49:53 -0300

    Alteração JS
    Alteração JS

--- GHBank/main.js
+++ GHBank/main.js
@@ -1,50 +1,7 @@
-//api key AIzaSyAsUXkx7GYMapDOVnKU4s_rmvgnYG1-Zgc
-
-const google = require('googleapis');
-const JWT = require('google-auth-library');
-
-const spreadsheetId = '1gor9whs1L1RTkQ_G3snfYJjbK_fR3_Q9FpVd7D4DuLM'; // Remova a parte de edição do URL
-const sheetName = 'BD_GHBank';
-const url = `https://docs.google.com/spreadsheets/d/1gor9whs1L1RTkQ_G3snfYJjbK_fR3_Q9FpVd7D4DuLM/edit?usp=sharing`
-
-let data = await fetch(url)
-    .then(res => res.text())
-    .then(text => JSON.parse(text.substring(47).slice(0,-2)))
-
-const auth = new JWT({
-    keyFile: 'path/to/your/service-account-key.json', // Substitua pelo caminho real do seu arquivo de chave
-    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-});
-
-auth.authorize().then(() => {
-    const sheets = google.sheets({ version: 'v4', auth });
-
-    sheets.spreadsheets.values.get({
-        spreadsheetId,
-        range: sheetName,
-    }).then((response) => {
-        const values = response.data.values;
-
-        // Adiciona um novo registro
-        values.push(['1234.56', 'Recebimento', '2023-12-12']);
-
-        sheets.spreadsheets.values.update({
-            spreadsheetId,
-            range: sheetName,
-            valueInputOption: 'RAW',
-            resource: {
-                values,
-            },
-        }).then(() => {
-            console.log('Registro adicionado com sucesso!');
-        }).catch((err) => {
-            console.error('Erro ao adicionar registro:', err);
+var saldo = "00,00"       
+       
+       // Certifique-se de que o script seja executado após o carregamento do DOM
+        document.addEventListener('DOMContentLoaded', function () {
+            // Atualize o conteúdo da span com o valor do saldo
+            document.getElementById('saldoValor').textContent = saldo;
         });
-    }).catch((err) => {
-        console.error('Erro ao obter valores da planilha:', err);
-    });
-});
-
-
-
-
