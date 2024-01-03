f4840963d7086fa11b98d700f54d1b985e39e5e5
Gabriel Hiago de Sousa <gabrielhiago96@hotmail.com>
2024-01-03 13:03:13 -0300

    Comit js 01
    Comit  JS 02

--- GHBank/main.js
+++ GHBank/main.js
@@ -1,7 +1,14 @@
-var saldo = "00,00"       
+let saldo = "00,00"       
        
        // Certifique-se de que o script seja executado após o carregamento do DOM
         document.addEventListener('DOMContentLoaded', function () {
             // Atualize o conteúdo da span com o valor do saldo
             document.getElementById('saldoValor').textContent = saldo;
         });
+
+        function realizarDeposito() {
+            var valorDeposito = document.getElementById('valorDeposito').value;
+            valorDeposito = parseFloat(valorDeposito);
+            saldo += valorDeposito;
+            document.getElementById('saldoValor').textContent = saldo;
+        }     
